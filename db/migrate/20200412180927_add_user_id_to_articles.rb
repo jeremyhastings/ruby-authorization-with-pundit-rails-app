58744e96942b7241a57fadd9659b94c4f87ff798
Jeremy Hastings <jeremy.hastings@mac.com>
2020-04-12 11:09:52 -0700

    rails generate migration add_user_id_to_articles user:references

--- db/migrate/20200412180927_add_user_id_to_articles.rb
+++ db/migrate/20200412180927_add_user_id_to_articles.rb
@@ -1,4 +1,4 @@
-class AddUserItToArticles < ActiveRecord::Migration[6.0]
+class AddUserIdToArticles < ActiveRecord::Migration[6.0]
   def change
     add_reference :articles, :user, null: false, foreign_key: true
   end

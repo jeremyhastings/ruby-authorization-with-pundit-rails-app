b5c718f9433cb7c7e8f1bbb386f8260793084a47
Jeremy Hastings <jeremy.hastings@mac.com>
2020-04-12 09:22:10 -0700

    root "pages#welcome"

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/welcome'
+
+  root "pages#welcome"
+
+  # get 'pages/welcome'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
